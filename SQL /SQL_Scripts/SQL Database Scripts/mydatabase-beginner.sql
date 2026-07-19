cfa9a3df86bcb1edc582f0804d1d8417ed87beb3
BhikkadIT <bhikkaditdataanalyst@gmail.com>
2026-07-19 14:00:02 +0530

    Add files via upload

---
+++ SQL /SQL_Scripts/SQL Database Scripts/mydatabase-beginner.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE MyDatabase;
+
+USE MyDatabase;
+
+DROP TABLE IF EXISTS customers;
+CREATE TABLE customers (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    country VARCHAR(50),
+    score INT
+);
+
+INSERT INTO customers (id, first_name, country, score) VALUES
+(1, 'Maria', 'Germany', 350),
+(2, 'John', 'USA', 900),
+(3, 'Georg', 'UK', 750),
+(4, 'Martin', 'Germany', 500),
+(5, 'Peter', 'USA', 0);
+
+DROP TABLE IF EXISTS orders;
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    sales INT
+);
+
+INSERT INTO orders (order_id, customer_id, order_date, sales) VALUES
+(1001, 1, '2021-01-11', 35),
+(1002, 2, '2021-04-05', 15),
+(1003, 3, '2021-06-18', 20),
+(1004, 6, '2021-08-31', 10);
